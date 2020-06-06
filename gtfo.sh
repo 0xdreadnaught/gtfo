48ae206c3ffe41af9ae69d9f530dadccda8b2d63
Jellyfish Sizzle <jellyfishsizzle@protonmail.com>
2020-06-05 18:45:35 -0700

    Sorted

--- gtfo.sh
+++ gtfo.sh
@@ -3,7 +3,7 @@
 # gtfo.sh
 #
 # Check gtfobins for privesc opportunities
-# Version: 2.0
+# Version: 2.1
 
 if [ $# -ne 1 ]; then
     echo "ERROR: List of binaries required"
@@ -12,7 +12,7 @@ if [ $# -ne 1 ]; then
 fi
 
 binfile=$1
-
+results=()
 binaries=($(cat ${binfile}))
 gtfobins=($(curl -k -s https://gtfobins.github.io | grep "<li>" | awk '{print $2}' | sed 's@href="/gtfobins/@@g' | sed s'@</a></li>@@g' | sed s'/>/ /g' | sed 's/"//g' | sed 's@/#@:@g' | awk '{print $1}'))
 for binary in "${binaries[@]}"
@@ -21,7 +21,9 @@ do
 	for gtfobin in "${gtfobins[@]}"
 	do
 		if [[ "$gtfobin" == "$bin:"* ]]; then
-			echo "$gtfobin"
+			results+=($gtfobin)
 		fi
 	done
 done
+
+echo "${results[*]}" | tr " " "\n" | sort
